01b4e6016888042b158df347a210f1596ba31ad6
Maicol <72515186+maicol240@users.noreply.github.com>
2024-04-18 02:51:37 -0400

    button events when a number is clicked

--- javascript.js
+++ javascript.js
@@ -1,15 +1,12 @@
 let screen = document.querySelector(".screen");
 
-const BUTTONS = document.querySelectorAll(".number");
+const NUMBERS = document.querySelectorAll(".number");
 
-BUTTONS.forEach((button) => {
-  button.addEventListener("click", () => {
-    let currentContext = screen.textContent;
-
-    if (currentContext == 0) {
-      currentContext = button.textContent;
-    } else currentContext += button.textContent;
-
-    screen.textContent = currentContext;
-  });
+//NUmber function when pressing a number
+NUMBERS.forEach((number) => {
+  //Number event listener
+  number.addEventListener("click", () =>
+    //add Number
+    {}
+  );
 });
